325631a8009e257c3f435a602c88e3a57cc89702
shu-kob <shukobuchi+github@gmail.com>
2018-05-08 14:04:47 +0000

    first commit

--- app/controllers/lightning_controller.rb
+++ app/controllers/lightning_controller.rb
@@ -1,12 +1,5 @@
-require 'bitcoin'
-require 'openassets'
 require 'net/http'
 require 'json'
-Bitcoin.network = :testnet3
-RPCUSER = "shu"
-RPCPASSWORD = "ubuntu1610"
-HOST="localhost"
-PORT=18332
 
 class LightningController < ApplicationController
     def index
@@ -24,33 +17,5 @@ class LightningController < ApplicationController
         @nodes_json = nodes_json['nodes']
 
         render template: 'lightning/index'
-    end
-
-    def connect
-
-    end
-
-    private
-    def bitcoinRPC(method,param)
-        http = Net::HTTP.new(HOST, PORT)
-        request = Net::HTTP::Post.new('/')
-        request.basic_auth(RPCUSER,RPCPASSWORD)
-        request.content_type = 'application/json'
-        request.body = {method: method, params: param, id: 'jsonrpc'}.to_json
-        JSON.parse(http.request(request).body)["result"]
-    end
-
-    def openassetsRPC()
-      api = OpenAssets::Api.new({
-          network:           'testnet',
-          rpc: {
-              user:              'shu',
-              password:          'ubuntu1610',
-              schema:            'http',
-              port:               18332,
-              host:              'localhost'}
-      }) 
-      utxo_list = api.list_unspent
-      JSON.pretty_generate(utxo_list)
     end
 end
